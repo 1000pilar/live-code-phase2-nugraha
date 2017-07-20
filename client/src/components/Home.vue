54c13ad4b2bf653c198d12b78cbecb632c3370d8
1000pilar <nunua7a8@gmailcom>
2017-07-20 15:15:58 +0700

    set vuex login & signin

---
+++ client/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <Navbar></Navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+export default {
+  components: {
+    Navbar
+  },
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div class="">
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Nugraha Vuex</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a @click="toSignin"><span class="glyphicon glyphicon-log-in"></span > Login</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right" v-if="islogin">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Welcome {{name}}</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> LogOut</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav-bar',
+  methods: {
+    toSignin () {
+      this.$router.push('/signin')
+    }
+  },
+  computed: {
+    islogin (){
+      return this.$store.state.isLogin
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
